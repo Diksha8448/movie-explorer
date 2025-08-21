6b87914376b411bb6be11526ee07c801a4139a50
Diksha <diksha.rajput@publicissapient.com>
2025-08-21 15:02:22 +0530

    movie card

---
+++ src/components/MovieCard.js
@@ -0,0 +1,22 @@
+// src/components/MovieCard.js
+import React from "react";
+import FavoriteButton from "./FavoriteButton";
+
+export default function MovieCard({ movie, onToggleFavorite, isFavorite }) {
+  return (
+    <div className="movie-card">
+      <img
+        src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/150"}
+        alt={movie.Title}
+      />
+      <h3>{movie.Title}</h3>
+      <p>{movie.Year}</p>
+
+      {/* Props drilling: Pass favorite state & handler down */}
+      <FavoriteButton
+        isFavorite={isFavorite}
+        onClick={() => onToggleFavorite(movie)}
+      />
+    </div>
+  );
+}
